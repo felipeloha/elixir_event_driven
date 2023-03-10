9d1da58f0262184ebcd43ae47b9743e6e6f13c20
felipe lopez <felipe.lopez@solarisbank.de>
2023-03-10 11:01:59 -0500

    setup on compose

--- setup_sqs_sns.sh
+++ setup_sqs_sns.sh
@@ -3,8 +3,10 @@ aws configure set aws_secret_access_key "dummy" --profile test-profile
 aws configure set region "eu-central-1" --profile test-profile
 aws configure set output "table" --profile test-profile
 
+HOST=localstack
+
 #create topic, queue and subscription
-aws --endpoint-url=http://localhost:4566 sns create-topic --name order-creation-events --region eu-central-1 --profile test-profile --output table | cat
-aws --endpoint-url=http://localhost:4566 sqs create-queue --queue-name dummy-queue --profile test-profile --region eu-central-1 --output table | cat
-aws --endpoint-url=http://localhost:4566 sns subscribe --topic-arn arn:aws:sns:eu-central-1:000000000000:order-creation-events --profile test-profile  --protocol sqs --notification-endpoint arn:aws:sqs:eu-central-1:000000000000:dummy-queue --output table | cat
+aws --endpoint-url=http://$HOST:4566 sns create-topic --name order-creation-events --region eu-central-1 --profile test-profile --output table | cat
+aws --endpoint-url=http://$HOST:4566 sqs create-queue --queue-name dummy-queue --profile test-profile --region eu-central-1 --output table | cat
+aws --endpoint-url=http://$HOST:4566 sns subscribe --topic-arn arn:aws:sns:eu-central-1:000000000000:order-creation-events --profile test-profile  --protocol sqs --notification-endpoint arn:aws:sqs:eu-central-1:000000000000:dummy-queue --output table | cat
 
