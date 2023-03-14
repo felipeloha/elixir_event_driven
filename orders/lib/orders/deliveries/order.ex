c85e31f8eb8395923f5cd895f89d4cfef5413313
Laura Hincapie <laura@pulpowms.com>
2023-03-13 20:48:43 -0500

    removed commented lines

--- orders/lib/orders/deliveries/order.ex
+++ orders/lib/orders/deliveries/order.ex
@@ -15,20 +15,7 @@ defmodule Orders.Deliveries.Order do
     order
     |> cast(attrs, [:name, :status])
     |> validate_required([:name])
-
-    # |> validate_status()
   end
-
-  #  defp validate_status(%{data: %{status: current}, changes: %{status: status}} = changeset),
-  #    do: validate_status(changeset, current, status)
-  #
-  #  defp validate_status(changeset), do: changeset
-  #
-  #  defp validate_status(changeset, "requested", "rejected"), do: changeset
-  #  defp validate_status(changeset, "requested", "confirmed"), do: changeset
-  #
-  #  defp validate_status(changeset, _, _),
-  #    do: add_error(changeset, :status, "invalid_status_transition")
 
   defmodule OrderStateMachine do
     alias Orders.Deliveries
